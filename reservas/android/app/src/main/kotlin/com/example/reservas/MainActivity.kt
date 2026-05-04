7657f545b49f77110ee36da3de54a586226c44f3
Mario <mariodanielq.p@gmail.com>
2026-05-04 09:10:38 -0500

    feat delete project

--- reservas/android/app/src/main/kotlin/com/example/reservas/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.reservas
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- reservas/lib/app/utils/env.dart
+++
@@ -1,3 +0,0 @@
-class Env {
-  static const String apiBaseURL = 'http://10.0.0.2.8000/api';
-}
